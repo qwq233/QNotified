c6502a744c461d2c7ddfe0d869cc3fe26f272c72
gao_cai_sheng <qwq233@qwq2333.top>
2020-10-03 21:28:01 +0800

    [skip ci]test

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -2,12 +2,6 @@
 
 commit=$(git show -s --format="%s %b")
 echo "commit: $commit"
-echo "trigger: $APPCENTER_TRIGGER"
-mkdir -p android/app/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
-if [[ $APPCENTER_TRIGGER == "manual" ]]; then
-    echo "Manual trigger, continue building..."
-    exit 0
-fi
 
 if [[ $commit == *"[skip ci]"* ]]; then
     curl -i -X PATCH -H "X-API-Token:$APPCENTER_API_TOKEN" -H "Content-Type: application/json" -d "{\"status\":\"cancelling\"}" https://appcenter.ms/api/v0.1/apps/qwq233/qn/builds/$APPCENTER_BUILD_ID
